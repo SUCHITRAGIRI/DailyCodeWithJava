251b72a5854c3108fcb1c206f26da6f78024b636
Suchitra Giri <suchigiri13@gmail.com>
2021-04-10 12:23:33 +0530

    Added merge sort file

---
+++ Sorting/StableSort/MergeSort.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int[] merge_sort(int[] input) {
+        if (input.length <= 1) {
+            return input;
+        }
+        int pivot = input.length / 2;
+        int[] left_list = merge_sort(Arrays.copyOfRange(input, 0, pivot));
+        int[] right_list = merge_sort(Arrays.copyOfRange(input, pivot, input.length));
+        return merge(left_list, right_list);
+    }
+
+    public int[] merge(int[] left_list, int[] right_list) {
+        int[] ret = new int[left_list.length + right_list.length];
+        int left_cursor = 0, right_cursor = 0, ret_cursor = 0;
+
+        while (left_cursor < left_list.length && right_cursor < right_list.length) {
+            if (left_list[left_cursor] < right_list[right_cursor]) {
+                ret[ret_cursor++] = left_list[left_cursor++];
+            } else {
+                ret[ret_cursor++] = right_list[right_cursor++];
+            }
+        }
+        // append what is remain the above lists
+        while (left_cursor < left_list.length) {
+            ret[ret_cursor++] = left_list[left_cursor++];
+        }
+        while (right_cursor < right_list.length) {
+            ret[ret_cursor++] = right_list[right_cursor++];
+        }
+        return ret;
+    }
+}
+
+class MergeSort {
+    public static void main(String args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // call merge sort function
+        arr = merge_sort(arr);
+        System.out.print(arr);
+    }
+}
