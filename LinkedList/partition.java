6518472df2ce749aa93bab13dfefc21aba5216fc
Suchitra Giri <suchigiri13@gmail.com>
2021-04-15 23:11:26 +0530

    Add file on LinkedList

---
+++ LinkedList/partition.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode result1 = new ListNode(-1);
+        ListNode result = result1;
+        ListNode temp = head;
+        while(temp != null){
+            if(temp.val < x){
+                result.next = new ListNode(temp.val);
+                result = result.next;
+                }
+            temp = temp.next;
+            }
+        temp = head;
+        while(temp != null){
+            if(temp.val >= x){
+                result.next = new ListNode(temp.val);
+                result = result.next;
+                }
+          temp = temp.next;
+            }
+        return result1.next;
+        
+    }
+}
