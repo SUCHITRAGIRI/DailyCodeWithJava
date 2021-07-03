b59f78ac81b41ef3e44c1193f510883fbead09b8
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-07-03 23:29:36 +0530

    Update TwoSum.java

--- Hashing/TwoSum.java
+++ Hashing/TwoSum.java
@@ -1,1 +1,21 @@
-nnnn
+//https://leetcode.com/problems/two-sum/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(target - nums[i])){
+                map.put(nums[i], i);                
+            }else{
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
+                break;
+            }
+        }
+             
+        return ans;
+    }
+}
