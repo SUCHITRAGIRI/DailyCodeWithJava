abd219819d29f82604320107c0d48904673dbda6
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-07-16 18:53:26 +0530

    Update Sort_An_Array_Ac_To_Other.java

--- Hashing/Sort_An_Array_Ac_To_Other.java
+++ Hashing/Sort_An_Array_Ac_To_Other.java
@@ -41,3 +41,27 @@ class Solution{
     }
 }
 
+
+//If our input range is small then 👇 (Amazing Solution)
+//https://leetcode.com/problems/relative-sort-array/
+
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] cnt = new int[1001];
+        for(int n : arr1) cnt[n]++;
+        int i = 0;
+        for(int n : arr2) {
+            while(cnt[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        for(int n = 0; n < cnt.length; n++) {
+            while(cnt[n]-- > 0) {
+                arr1[i++] = n;
+            }
+        }
+        return arr1;
+    }
+
+}
+
