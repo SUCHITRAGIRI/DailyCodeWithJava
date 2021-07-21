4bdd4669dbaa484b110543361a61f96cb9e5b4ed
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-07-21 22:48:23 +0530

    Update Longest_Substring_Without_Repeating_Char.java

--- Hashing/Longest_Substring_Without_Repeating_Char.java
+++ Hashing/Longest_Substring_Without_Repeating_Char.java
@@ -10,7 +10,8 @@
 //         and update the len should be from left to right so, we can simply write (right - left + 1)
 //         if contains then upadate the left pointer (means left pointer is just used for maintainig the max lenth as well as avoid duplicacy)
    
-          
+ 
+//time 4ms
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int left = 0, right = 0, len = 0;
@@ -26,3 +27,39 @@ class Solution {
         return len;
     }
 }
+
+//More efficient 2ms
+class Solution {
+    
+    public int lengthOfLongestSubstring(String s) {
+        int result = 0;
+        int[] cache = new int[256];
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+            cache[s.charAt(i)] = i + 1;
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
+    }
+}
+
+//Using queue but inefficient
+//TC: O(N^2)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Queue<Character> queue = new LinkedList<>();
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            while (queue.contains(c)) {
+                queue.poll();
+            }
+            queue.offer(c);
+            res = Math.max(res, queue.size());
+        }
+        return res;
+    }
+}    
+    
+
+    
+
