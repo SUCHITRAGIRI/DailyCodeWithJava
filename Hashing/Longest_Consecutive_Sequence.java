1583dcc9eb09c2988c544a4a33b4585bcf78d9fc
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-07-22 10:53:34 +0530

    Update Longest_Consecutive_Sequence.java

--- Hashing/Longest_Consecutive_Sequence.java
+++ Hashing/Longest_Consecutive_Sequence.java
@@ -51,3 +51,30 @@ class Solution {
         return longestStreak;
     }
 }
+
+//Another solution
+public int longestConsecutive(int[] num) {
+    int res = 0;
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int n : num) {
+        if (!map.containsKey(n)) {
+            int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
+            int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+            // sum: length of the sequence n is in
+            int sum = left + right + 1;
+            map.put(n, sum);
+            
+            // keep track of the max length 
+            res = Math.max(res, sum);
+            
+            // extend the length to the boundary(s)
+            // of the sequence
+            // will do nothing if n has no neighbors
+            map.put(n - left, sum);
+            map.put(n + right, sum);
+        }
+        else {
+            // duplicates
+            continue;
+        }
+    }
