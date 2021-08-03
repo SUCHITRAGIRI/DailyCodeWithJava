329287da4d4dac83e850c56e2c21e831691dbef8
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-08-03 23:47:04 +0530

    Update SubsetII.java

--- Backtracking/SubsetII.java
+++ Backtracking/SubsetII.java
@@ -79,6 +79,33 @@ public List<List<Integer>> subsetsWithDup(int[] num) {
 
 //Bit manipulation
 
+  public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> lists = new ArrayList<>();
+        int subsetNum = 1<<num.length;
+        for(int i=0;i<subsetNum;i++){
+            List<Integer> list = new ArrayList<>();
+            boolean illegal=false;
+            for(int j=0;j<num.length;j++){
+                if((i>>j&1)==1){
+                    if(j>0&&num[j]==num[j-1]&&(i>>(j-1)&1)==0){
+                        illegal=true;
+                        break;
+                    }else{
+                        list.add(num[j]);
+                    }
+                }
+            }
+            if(!illegal){
+               lists.add(list); 
+            }
+            
+        }
+        return lists;
+    }
+}
+
 
 
 
