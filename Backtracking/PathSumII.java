8b25e817962707986d365e3cefae626cbeada717
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-08-04 23:03:06 +0530

    Update PathSumII.java

--- Backtracking/PathSumII.java
+++ Backtracking/PathSumII.java
@@ -22,3 +22,23 @@ class Solution {
         path.remove(path.size() - 1);
     }
 }
+
+//
+private List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+public List<List<Integer>> pathSum(TreeNode root, int sum) {
+    helper(new ArrayList<Integer>(), root, sum);
+    return result;
+}
+
+private void helper(List<Integer> list, TreeNode root, int sum) {
+    if (root == null) return;
+    list.add(root.val);
+    sum -= root.val;
+    if (root.left == null && root.right == null) {
+        if (sum == 0) result.add(list);
+        return;
+    }
+    helper(new ArrayList<Integer>(list), root.left, sum);
+    helper(new ArrayList<Integer>(list), root.right, sum);
+}
