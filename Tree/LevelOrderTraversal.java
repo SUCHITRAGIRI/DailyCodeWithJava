44dd18fde390dddb41e3bd70202a07f5a0b0035b
SUCHITRAGIRI <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-08-06 16:21:07 +0530

    Update LevelOrderTraversal.java

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -50,6 +50,29 @@ class LevelOrderTraversal {
 
 }
 
+//Another solution!
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<List<Integer>> ans = new ArrayList();
+        if(root == null){
+            return ans;
+        }
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> subList = new ArrayList<>();
+            for(int i = 0;i < size;i++){
+                if(q.peek().left != null)q.offer(q.peek().left);
+                if(q.peek().right != null)q.offer(q.peek().right);
+                subList.add(q.poll().val);
+            }
+            ans.add(subList);
+        }
+        return ans;  
+    }
+}
+
 
 
 
