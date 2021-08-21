22abb0428d936377d5672e2a1a3f74ebd7ea0765
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-08-21 18:47:44 +0530

    Update ValidSudoku.java

--- Backtracking/ValidSudoku.java
+++ Backtracking/ValidSudoku.java
@@ -2,6 +2,46 @@
 
 //Check where the existing elements are present in sudoku is valid or not!
 
+
+//My very 1st approach
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    if(!helper(board, board[i][j], i, j))return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean helper(char[][] board, char c, int row, int col){
+        
+        for(int i = 0; i < 9; i++){
+            if(board[row][i] == c && i != col)return false;          
+            if(board[i][col] == c && i != row)return false;
+            }
+        
+        //leftRow = row / 3 * 3;
+        
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        
+        for(int i = startRow; i < startRow + 3; i++){
+            for(int j = startCol; j < startCol + 3; j++){
+                if(board[i][j] == c && i != row && j != col)return false;
+            }
+        }
+        
+        return true;
+            
+    }
+}
+
+///############################################################################
+
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         for(int i = 0; i < 9; i++){
