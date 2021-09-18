881f3248ff16712f6b5c33da8012d950c81e6b61
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-09-19 00:00:54 +0530

    Update FirstUpperCaseLetter.java

--- Recursion/FirstUpperCaseLetter.java
+++ Recursion/FirstUpperCaseLetter.java
@@ -1,3 +1,26 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	String s = "TgeVekHsforgeeks";
+	char c = find1stUpper(s, 0);
+	if( c == '0') 
+	System.out.println("Upper case is not found"); 
+	else
+	System.out.println(c);
+	}
+	
+	static char find1stUpper(String s, int ind){
+	        if(ind == s.length())return '0';
+	        //if(s.charAt(ind) >= 65 &&  s.charAt(ind) <= 90)return s.charAt(ind);
+	        if(Character.isUpperCase(s.charAt(ind)))return s.charAt(ind);
+	        return find1stUpper(s, ind + 1);
+	}
+}
+
+//################# Iterative
 import java.io.*;
 
 class GFG {
