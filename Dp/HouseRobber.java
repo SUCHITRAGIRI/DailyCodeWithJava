ba4d355bd56542a91b76f56be90ca925cf5edd20
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-09-29 22:53:03 +0530

    Create HouseRobber.java

---
+++ Dp/HouseRobber.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/house-robber/discuss/156523/From-good-to-great.-How-to-approach-most-of-DP-problems.
+
+//My first approach and I am very glad to see it works:)
+//Really makes me Happy
+//Top Down
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1)return nums[0];
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return Math.max(helper(nums, n - 1, dp), helper(nums, n - 2, dp));
+    }
+    
+    public int helper(int[] nums, int n, int[] dp){
+        if(n < 0)return 0;
+        if(n == 0 || n == 1)return nums[n];
+        if(dp[n] != -1)return dp[n];
+        return dp[n] = nums[n] + Math.max(helper(nums, n - 2, dp), helper(nums, n - 3, dp));
+        
+    }
+}
+
+//Bottom Up
