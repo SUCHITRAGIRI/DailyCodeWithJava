90a4561d62c042ec716275ed9e12e20c273ab4e2
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-09-29 23:12:42 +0530

    Update HouseRobber.java

--- Dp/HouseRobber.java
+++ Dp/HouseRobber.java
@@ -22,3 +22,18 @@ class Solution {
 }
 
 //Bottom Up
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1)return nums[0];
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 1; i < n; i++){
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        return dp[n];
+    }
+    
+}
