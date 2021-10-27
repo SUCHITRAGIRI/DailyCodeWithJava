3bb6582fc5c631db7fb2f12cb47e851f38ea3664
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-10-27 19:05:04 +0530

    Update MergeSort.java

--- Sorting/StableSort/MergeSort.java
+++ Sorting/StableSort/MergeSort.java
@@ -1,3 +1,6 @@
+//Time complexity: O(NlogN)
+//space: O(N)
+
 import java.util.*;
 
 class Solution {
@@ -44,5 +47,55 @@ class MergeSort {
         // call merge sort function
         arr = merge_sort(arr);
         System.out.print(arr);
+    }
+}
+
+//another approach
+class Solution {
+    public int[] sortArray(int[] nums) {
+        //sort the array using merge sort
+        if(nums.length == 1)return nums;
+
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    void mergeSort(int[] arr, int start, int end){
+
+        if(start < end){
+            int mid = start + (end - start) / 2;
+
+            mergeSort(arr, start, mid);
+            mergeSort(arr, mid + 1, end);
+            merge(arr, mid, start, end);
+        }
+    }
+
+    void  merge(int[] arr, int mid, int start, int end){
+
+        int l = start, r = mid + 1, i = 0;
+
+        int[] mix = new int[end - start + 1];
+        while(l <= mid && r <= end){
+            if(arr[l] <= arr[r]){
+                mix[i++] = arr[l];
+                l++;
+            }else {
+                mix[i++] = arr[r];
+                r++;
+            }
+        }
+        while(l <= mid){
+            mix[i++] = arr[l++];
+        }
+
+        while(r <= end){
+            mix[i++] = arr[r++];
+        }
+
+        for(int k = 0; k < mix.length; k++){
+            arr[start + k] = mix[k];
+        }
+
     }
 }
