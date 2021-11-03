93bd35d914c35856660a9d9b87096bc624963ee2
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-11-03 19:12:58 +0530

    Create Richest-Customer-wealth.java

---
+++ Arrays/Richest-Customer-wealth.java
@@ -0,0 +1,35 @@
+//T: O(mn)
+//S: O(1)
+
+  //Variation 1: Using loops
+
+public int maximumWealth(int[][] accounts) {
+	int max = 0;
+	for (int[] customer : accounts) {
+		int wealth = 0;
+		for (int account : customer)
+			wealth += account;
+		max = Math.max(max, wealth);
+	}
+	return max;
+}
+
+//Variation 2: Using Loop and Stream
+
+public int maximumWealth(int[][] accounts) {
+	int max = 0;
+	for (int[] customer : accounts) {
+		int wealth = Arrays.stream(customer).sum();
+		max = Math.max(max, wealth);
+	}
+	return max;
+}
+
+//Variation 3: Using Streams
+
+public int maximumWealth(int[][] accounts) {
+	return Arrays.stream(accounts)
+				 .mapToInt(customerAccount -> Arrays.stream(customerAccount).sum())
+				 .max()
+				 .getAsInt();
+}
