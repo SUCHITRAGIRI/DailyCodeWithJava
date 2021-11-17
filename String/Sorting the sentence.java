4115b1f06b999964b8d86343df9884cef1b57ba5
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-11-17 22:42:28 +0530

    Create Sorting the sentence.java

---
+++ String/Sorting the sentence.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/sorting-the-sentence/
+
+
+//Time: O(N), where N is the length of string
+//Note: better to use StringBuilder for great efficiency cuz if we use string and '+' operator try to append the sentence it is taking 4ms, but in contrast if we use the String Builder then it is taking 0ms!
+
+class Solution{
+    public static String sortSentence(String s) {
+	String[] str = s.split(" ");                // Since the String contains only whitespaces, it's safe to split the String at them
+	String[] res = new String[str.length];      // This String array will hold the rearranged words
+	StringBuilder sb = new StringBuilder();     // Instead of using '+' operator to concat repeatedly, I have created a StringBuilder object which is more efficient
+	int i = 0;                          
+	for (String elem : str) {
+		i = (int) (elem.charAt(elem.length() - 1) - '0');  // This  will extract the digit present at the end of each String and store it in i (i = index of the word in res[])
+		res[i - 1] = elem.substring(0, elem.length() - 1); // This gives the substring of each String except for the digit in the end and stores the word at an appropriate index (i - 1) in res[]
+	}
+	//append the words from res[] to StringBuilder object to form a sentence
+	for (i = 0; i < res.length - 1; i++)
+		sb.append(res[i]).append(" ");
+	  sb.append(res[i]);
+	return sb.toString();
+}
+}
