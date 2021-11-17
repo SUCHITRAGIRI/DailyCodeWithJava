ed6d82f1c71c004e08ed4cf5db762ed6ec57ee87
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-11-17 22:56:00 +0530

    Update Sorting the sentence.java

--- String/Sorting the sentence.java
+++ String/Sorting the sentence.java
@@ -37,3 +37,14 @@ class Solution {
         return String.join(" ", bucket);
     }
 }
+
+//
+class Solution{
+public String sortSentence(String s) {
+        String[] ans = new String[s.split(" ").length];
+        for(String st: s.split(" ")){
+            ans[st.charAt(st.length()-1) - '1'] = st.substring(0,st.length()-1);
+        }
+        return String.join(" ", ans);
+    }
+}
