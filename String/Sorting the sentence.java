33e888f5ba0c0a200bcb69a36eead74653c52aa1
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2021-11-17 22:50:43 +0530

    Update Sorting the sentence.java

--- String/Sorting the sentence.java
+++ String/Sorting the sentence.java
@@ -21,3 +21,19 @@ class Solution{
 	return sb.toString();
 }
 }
+
+//
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] bucket = new String[words.length];
+        for (String word : words) {
+            // Get 0th-based index from the word
+            int index = Character.getNumericValue(word.charAt(word.length() - 1)) - 1;
+            // Store the original word without a numeric value
+            bucket[index] = word.substring(0, word.length() - 1);
+        }
+        
+        return String.join(" ", bucket);
+    }
+}
