d52b99be58ce27852d97d2c499e303d55c719e81
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2022-01-29 11:55:36 +0530

    Update ReverseLinkedList.java

--- LinkedList/ReverseLinkedList.java
+++ LinkedList/ReverseLinkedList.java
@@ -86,3 +86,14 @@ class Solution
         return prev;
     }
 }
+
+
+//recursive approach
+        if(head==null|| head.next==null)return head;
+       
+        
+        ListNode node=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        
+        return node;
