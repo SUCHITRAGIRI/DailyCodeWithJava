4d82722795499ef7df5ffaee4c7e009d895f5852
Suchitra Giri <60110218+SUCHITRAGIRI@users.noreply.github.com>
2022-01-29 10:37:19 +0530

    Update DeleteTheGivenNode.java

--- LinkedList/DeleteTheGivenNode.java
+++ LinkedList/DeleteTheGivenNode.java
@@ -1,5 +1,5 @@
 //https://practice.geeksforgeeks.org/problems/delete-without-head-pointer/1/?company[]=Amazon&company[]=Amazon&page=1&query=company[]Amazonpage1company[]Amazon#
-//Here the problem is the we have given the refernece of the deleted node and we don't given the head refernce so, our task is to delete that perticular node.
+//Here the problem is that we have given the refernece of the deleted node and we don't given the head refernce so, our task is to delete that perticular node.
 
 //It seems really complex ..
 //but solution is dumpp.. XD
@@ -10,7 +10,7 @@ class Solution
     void deleteNode(Node del)
     {
          Node curr = del.next; //take a refernce of next node
-         del.data = curr.data; // copy the value of curr in give node
+         del.data = curr.data; // copy the value of curr in given node
          del.next = curr.next;// just delete the next node
          
     }
